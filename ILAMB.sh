05e91824c3e4d4d31dc0f3131a323719b1a2ca77
Ana Ordonez <ordonez4@llnl.gov>
2021-11-05 14:47:50 -0700

    mk and rm tmp

--- ILAMB.sh
+++ ILAMB.sh
@@ -2,19 +2,24 @@
 
 # Script assumes that cmec-driver is installed in current environment
 
+ilamb_version="2.6"
+
 # Download ILAMB release temporarily
+mkdir tmp
 cd tmp
-wget https://github.com/rubisco-sfa/ILAMB/archive/refs/tags/v2.6.tar.gz
+wget https://github.com/rubisco-sfa/ILAMB/archive/refs/tags/v${ilamb_version}.tar.gz
 tar -xvf v2.6.tar.gz
 
 # Copy only the needed files to ILAMB folder
 mkdir ../ILAMB
-cp ILAMB-2.6/contents.json ../ILAMB
-cp -r ILAMB-2.6/cmec-driver ../ILAMB
+cp ILAMB-${ilamb_version}/contents.json ../ILAMB
+cp -r ILAMB-${ilamb_version}/cmec-driver ../ILAMB
 
 # Create ILAMB conda environment
 conda create -n _CMEC_ilamb -c conda-forge ILAMB
 
 # Cleanup
 rm -rf ILAMB-2.6
+cd ../
+rm -rf tmp
 
